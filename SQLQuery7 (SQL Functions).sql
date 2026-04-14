9c1ddbe1e1a6bf96a13522cc4b67645d61423a8d
RONIT PATEL <ronitpatel1810@gmail.com>
2026-04-14 23:20:23 +0530

    Done_String_function

--- SQLQuery7 (SQL Functions).sql
+++ SQLQuery7 (SQL Functions).sql
@@ -17,3 +17,57 @@ SELECT
     LOWER(first_name) AS lower_first_name,
     UPPER(country) AS upper_country
 FROM customers;
+
+--3. TRIM
+-- Removes leading and trailing spaces from the first name.
+SELECT --used to find the records where there are leading or trailing spaces in the first name
+    first_name
+FROM customers
+WHERE first_name != TRIM(first_name)
+
+SELECT
+    first_name,
+    TRIM(first_name) AS trimmed_first_name
+FROM customers;
+
+
+
+--4. replace
+-- Replace .txt with .pdf in the file_name column.
+SELECT
+    'report.txt' AS old_file_name, --hardcoded value for demonstration
+    REPLACE('report.txt', '.txt', '.pdf') AS new_file_name;
+
+
+--Calculation Functions
+--LEN
+-- Returns the length of the first name.
+SELECT
+    first_name,
+    LEN(first_name) AS name_length
+FROM customers;
+
+
+
+--String Functions
+--LEFT and RIGHT
+-- Extracts the first 3 characters of the first name and the last 3 characters of the country.
+SELECT
+    first_name,
+    country,
+    LEFT(TRIM(first_name), 3) AS left_first_name,
+    RIGHT(country, 3) AS right_country
+FROM customers;
+
+
+--SUBSTRING
+-- Extracts a substring from the first name starting at the second character and of length 4.
+SELECT
+    first_name,
+    SUBSTRING(TRIM(first_name), 2, 4) AS substring_first_name --(string, start_position, length)
+FROM customers;
+--If the length is not specified
+SELECT
+    first_name,
+    SUBSTRING(TRIM(first_name), 2, LEN(TRIM(first_name))) AS substring_first_name --(string, start_position)
+FROM customers;
